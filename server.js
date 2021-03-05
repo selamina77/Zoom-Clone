0519a9b965ab67f2970c5f21694ed6fd7fe902a6
selamina <selamsolomon84@yahoo.com>
2021-03-05 00:49:57 -0600

    zoom-clone

--- server.js
+++ server.js
@@ -2,8 +2,6 @@
 
 const express = require('express')
 const app = express()
-// const cors = require('cors')
-// app.use(cors())
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
 const { ExpressPeerServer } = require('peer');
