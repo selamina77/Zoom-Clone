cfeffc159dc6ed6fba6d1bcf75b0c35f41e74b38
selamina <selamsolomon84@yahoo.com>
2021-03-05 00:21:26 -0600

    update

--- server.js
+++ server.js
@@ -1,67 +1,9 @@
 
 
-// const express = require('express')
-// const app = express()
-// const server = require('http').Server(app)
-// const io = require('socket.io')(server)
-// const { ExpressPeerServer } = require('peer');
-// const peerServer = ExpressPeerServer(server, {
-//   debug: true,
-// });
-// const { v4: uuidV4 } = require('uuid')
-
-// app.use('/peerjs', peerServer);
-
-// app.set('view engine', 'ejs')
-// app.use(express.static('public'))
-
-// app.get('/', (req, res) => {
-//   res.redirect(`/${uuidV4()}`)
-// })
-
-// app.get('/:room', (req, res) => {
-//   res.render('room', { roomId: req.params.room })
-// })
-
-// io.on('connection', socket => {
-//   socket.on('join-room', (roomId, userId) => {
-//     socket.join(roomId)
-//     socket.to(roomId).broadcast.emit('user-connected', userId);
-   
-//     // socket.on('disconnect', () => {
-//     //   socket.to(roomId).broadcast.emit('user-disconnected', userId)
-//     // })
-       
-//     });
-//   });
-
-
-// server.listen(3000);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 const express = require('express')
 const app = express()
-const cors = require('cors')
-app.use(cors())
+// const cors = require('cors')
+// app.use(cors())
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
 const { ExpressPeerServer } = require('peer');
@@ -100,3 +42,13 @@ io.on('connection', socket => {
 })
 
 server.listen(process.env.PORT||3030)
+
+
+
+
+
+
+
+
+
+
